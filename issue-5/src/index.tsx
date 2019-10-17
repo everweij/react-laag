d71f1bec536952f5166b5cca2bd93df91831383d
Erik <erik@hubper.co>
2019-10-18 00:01:31 +0200

    removed sandbox

--- issue-5/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { render } from "react-dom";
-import { Example } from "./Example";
-
-import "./styles.css";
-
-const App = () => <Example />;
-
-render(<App />, document.getElementById("root"));
