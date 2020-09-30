e0df1ded4bc562226ef197c99e362b432efe933c
Maxime Fontaine Bombardier <Maxime.Fontaine-Bombardier@bentley.com>
2020-09-30 16:47:49 -0400

    Remove utility hooks from root index file, Add new index for utility hooks in ToggleLayer folder.

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,9 @@ export { default as useHover } from "./useHover";
 export { default as useBreakpoint } from "./useBreakpoint";
 export { default as Transition } from "./Transition";
 export { default as useTooltip } from "./useTooltip";
-export { default as useOutsideClick } from "./ToggleLayer/useOutsideClick";
-export { default as useOnScroll } from "./ToggleLayer/useOnScroll";
-export { default as useOnWindowResize } from "./ToggleLayer/useOnWindowResize";
-export { default as useTrackElementResize } from "./ToggleLayer/useTrackElementResize";
-export { default as useStyleState } from "./ToggleLayer/useStyleState";
-export { default as useIsOpen } from "./ToggleLayer/useIsOpen";
-export { default as useEvent } from "./ToggleLayer/useEvent";
-export { default as useElementRef } from "./ToggleLayer/useElementRef";
-export { default as useElementState } from "./ToggleLayer/useElementState";
-export { default as useIsomorphicLayoutEffect } from "./ToggleLayer/useIsomorphicLayoutEffect";
 
 export type { TransitionProps } from "./Transition";
-export type {
-  HoverOptions,
-  CallbackHoverOptions,
-  HoverProps,
-} from "./useHover";
+export type { HoverOptions, CallbackHoverOptions, HoverProps } from "./useHover";
 export type { TooltipOptions } from "./useTooltip";
 export type {
   AnchorEnum,
